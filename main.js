57145abedb87882e98b7c12b1a545cee40698872
emilhajizada <emil.hajizada@code.edu.az>
2022-11-10 13:20:44 +0400

    Added

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+const arr=["Amil","Shahin","Kamran","Rashad"]
+const arr1=[1,5,2,6,7]
+
+console.log(arr1.every((val)=>val<2));
+
+const str="This is string"
+
+console.log(str.replace("i","number"));
